02e7af5fa2e0f492ec204cc88f18b2784e9695e4
KoalaYan <1050335889@sjtu.edu.cn>
2020-04-18 08:28:25 +0800

    maybe final version

---
+++ Problem3/jni/parentAndChildren.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/syscall.h>
+#include<unistd.h>
+#include<string.h>
+
+#define __NR_pstreecall 356
+
+int main(){
+    //printf("debugger0.\n");
+    pid_t children_pid = fork();
+    //printf("debugger1.\n");
+    if(children_pid < 0){
+        //printf("debugger2.\n");
+        printf("Fail to fork.\n");
+    }
+    else if(children_pid == 0){
+        //printf("debugger3.\n");
+        pid_t pid = getpid();
+        printf("518030910094 Children %d.\n",pid);
+        execl("/data/misc/ptreeARM",NULL);
+    }
+    else{
+        //printf("debugger4.\n");
+        pid_t pid = getpid();
+        printf("518030910094 Parent%d.\n",pid);
+    }
+
+    return 0;
+}
